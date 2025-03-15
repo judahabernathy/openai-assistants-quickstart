bbf211aa77eb0c8b98a961389af6dbb7b08e174f
judahabernathy <judahabernathy@gmail.com>
2025-03-14 23:19:50 -0400

    Update assistant-config.ts

--- app/assistant-config.ts
+++ app/assistant-config.ts
@@ -1,5 +1,3 @@
-export let assistantId = ""; // asst_G3YhFH5MCu6PKOhGHVefg4Qk
+const ASSISTANT_ID = "asst_G3YhFH5MCu6PKOhGHVefg4Qk";
 
-if (assistantId === "") {
-  assistantId = process.env.OPENAI_ASSISTANT_ID;
-}
+export default ASSISTANT_ID;
