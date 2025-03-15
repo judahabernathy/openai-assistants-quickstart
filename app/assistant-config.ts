2579c7e2d2367441220b94083f9ba989fb9fbb7f
judahabernathy <judahabernathy@gmail.com>
2025-03-14 23:01:00 -0400

    Update assistant-config.ts
    added assistants id

--- app/assistant-config.ts
+++ app/assistant-config.ts
@@ -1,4 +1,4 @@
-export let assistantId = ""; // set your assistant ID here
+export let assistantId = ""; // tynqi3-juqjih-Sykgux
 
 if (assistantId === "") {
   assistantId = process.env.OPENAI_ASSISTANT_ID;
